31eb1d235efa5cf9fab1521a1619d809838f86ea
tobor <rosborne@osbornepro.com>
2019-08-24 16:22:23 -0600

    Create getip.sh
    Initial Commit for getip.sh

---
+++ getip.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Getting IP Addresses, Please Wait..."
+
+public=$(curl -s http://whatismijnip.nl | cut -d " " -f 5)
+
+private=$(ifconfig | grep 'inet ' | awk {'print $2'})
+
+if [ "$public" != "" ]
+then
+	echo "=========================" 
+	echo "Public: "
+	echo "-------------------------"
+	echo "$public"
+else
+	echo "Public IP address could not be found."
+fi
+
+echo "========================="
+echo "Private: "
+echo "-------------------------"
+echo "$private"
+echo "========================="
