467a8d6c5a3793963500375f94cbc8e8d82943a2
tobor <rosborne@osbornepro.com>
2019-08-25 20:35:56 -0600

    Create suidcheck.sh
    Initial Commit for suidcheck.sh

---
+++ suidcheck.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# This script is for finding commands with root SUID permissions and printing info for possible exploitation. 
+# Cronjobs are also displayed and the contents of the cronjobs are displayed if the contain commands.
+
+# FUTURE ADDS: If an exploitable SUID command is available, auotmoatically exploit it.
+
+
+#suid=$(/usr/bin/find / -perm -u=s -type f 2>/dev/null)
+
+exploit_cmds=(nmap vim find netcat less cp)
+
+/bin/echo "=================================================================="
+/bin/echo "|            LINUX COMMON PRIVESC CHECK RESULTS                  |"
+/bin/echo "=================================================================="
+
+for i in "${exploit_cmds[@]}"
+do 
+	results=$(/bin/grep -q "${i}" <<< "$suid" && /usr/bin/printf "$i root method is possibly exploitable!\n" || /usr/bin/printf "$i ""root method is not exploitable\n")
+	echo "${results[*]}"
+done
+
+/bin/echo "=================================================================="
+/bin/echo "|           SUID File List Located in PWD File suid             |"
+/bin/echo "------------------------------------------------------------------"
+#/bin/echo "$suid" > "${pwd}\suid" # Outputs the SUID results that were found
+/bin/echo "=================================================================="
+/bin/echo "|           GUID File List Located in PWD File guid              |"
+/bin/echo "=================================================================="
+#/bin/echo "$(/usr/bin/find -user root -perm 2000 -print 2> /dev/null)" > "${pwd}\guid" # Outputs the GSUID results that were found
+/bin/echo "=================================================================="
+/bin/echo "|                   CRON JOBS ON MACHINE                         |"
+/bin/echo "|----------------------------------------------------------------|"
+/bin/echo "$(/bin/ls --color=auto -las /etc/cron.d)"
+/bin/echo "=================================================================="
+/bin/echo "|                CRON JOB SCRIPT CONTENTS                        |"
+/bin/echo "------------------------------------------------------------------"
+/usr/bin/find /etc/cron.d -type f -exec cat {} \; | /usr/bin/awk '$1 ~ /^[^;#]/' | /bin/sed G   
+/bin/echo "=================================================================="
