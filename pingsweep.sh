da11af9c74976cc45b42ca8831f2b2978ed17bb6
tobor <rosborne@osbornepro.com>
2019-11-21 22:25:10 -0700

    Update pingsweep.sh

--- pingsweep.sh
+++ pingsweep.sh
@@ -5,7 +5,8 @@
 
 if [ -z "$1" ]
 then
-	echo "Use -h switch to view help information\n"
+	echo "Use -h switch to view help information"
+	echo ""
 else
 	if [ "$1" == '-h' ]; then
 		# This option displays a help message and command execution examples
@@ -15,23 +16,46 @@ else
 		echo "USAGE: pingsweep [network] "
 		echo ""
 		echo "OPTIONS:"
-		echo " -h : Displays the help information for the command."
+		echo "  -h : Displays the help information for the command."
 		echo ""
 		echo "EXAMPLES:"
 		echo "  pingsweep 192.168.0"
+		echo "  # This example performs a ping sweep from 192.168.0.1 to 254"
+		echo ""
+		echo "  pingsweep 192.168.0 128 192"
+		echo "  # This example performs a ping sweep from 192.168.0.128 to 192"
+		echo ""
+
 	elif [ -n "$1" ]; then
-			echo "=================="
-			echo "|  Active Hosts  |"
-			echo "=================="
-			for i in {1..254}
-			do
-				host=$(echo $1.$i)
-				fping -c1 -t200 $host 2>/dev/null 1>/dev/null
-				# fping's -t option is in miliseconds and can be modified to take loner or shorter. My goal here is speed.
-				if [ "$?" = 0 ]
-				then
-					echo $host
-				fi
-			done
+
+			if [ -n "$1" ] && [ -n "$2" ] && [ -z "$3" ]; then
+				echo "You are required to define a START and END IP Address range. Leave blank to sweep /24 subnet"
+				echo ""
+				exit
+			fi
+
+			if [ -z "$2" ]; then
+				START=1
+			elif [ -n "$2" ]; then
+				START=$2
+			fi
+
+			if [ -z "$3" ]; then
+				END=254
+			elif [ -n "$3" ]; then
+				END=$3
+			fi
+			
+			echo "Active Hosts"
+			
+			for i in $(seq $START $END); do
+				HOST=$(echo $1.$i)
+					fping -c1 -t200 $HOST 2>/dev/null 1>/dev/null
+					# fping's -t option is in miliseconds and can be modified to take loner or shorter. My goal here is speed.
+					if [ "$?" = 0 ]
+					then
+						echo $HOST
+					fi
+				done
 	fi	
 fi
