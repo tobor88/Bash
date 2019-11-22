49b72e4ab3024ca18df8cf210ecc984a5757e68a
tobor <rosborne@osbornepro.com>
2019-11-22 09:57:52 -0700

    Update pingsweep.sh

--- pingsweep.sh
+++ pingsweep.sh
@@ -32,53 +32,57 @@ else
 		echo ""
 
 	# Variable validation------------------------------------------------
-	elif [[ "$1" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] || printf "Valid IP subnet was not defined. For more help execute 'pingsweep -h' Example 172.16.32\n\n "; then
-	
+	elif [[ "$1" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] || ERROR="Valid IP subnet was not defined. For more help execute 'pingsweep -h' Example 172.16.32 "; then
+		
+		# Validate first parameter was defined correctly
+		if [ ! $ERROR ]; then
 			# Validate correct amount of positional parameters are defined
 			if [ -n "$4" ]; then
 				echo "Too many positional parameters have been defined. Execute 'pingsweep -h' for more options"
 				echo ""
 				exit
 			fi
+		else
+			echo $ERROR
+			exit
+		fi
 
-
-			# Validate positional parameter 2 is an integer between 1 and 254
-			if [ -z "$2" ]; then
-				START=1
-			elif [ "$2" -lt 255 ] && [ "$2" -ge 1 ] || ERROR="Second parameter needs to be an integer between 1 and 254"; then
-				if [ ! $ERROR ]; then
-					START=$2
-				else
-					echo $ERROR
-					exit
-				fi
+		# Validate positional parameter 2 is an integer between 1 and 254
+		if [ -z "$2" ]; then
+			START=1
+		elif [ "$2" -lt 255 ] && [ "$2" -ge 1 ] || ERROR="Second parameter needs to be an integer between 1 and 254"; then
+			if [ ! $ERROR ]; then
+				START=$2
+			else
+				echo $ERROR
+				exit
 			fi
-
+		fi
 			
-			# Validate positional parameter 3 is an integer between $2 and 254
-			if [ -z "$3" ]; then
-				END=254
-			elif [ "$3" -lt 255 ] || ERROR="Third parameter needs to be an integer between the value of positional parameter two and 254"; then
-				if [ ! $ERROR ]; then
-					END=$3
-				else
-					echo $ERROR
-					exit
-				fi
+		# Validate positional parameter 3 is an integer between $2 and 254
+		if [ -z "$3" ]; then
+			END=254
+		elif [ "$3" -lt 255 ] && [ "$3" -gt "$2" ] || ERROR="Third parameter needs to be an integer between the value of positional parameter two and 254"; then
+			if [ ! $ERROR ]; then
+				END=$3
+			else
+				echo $ERROR
+				exit
 			fi
+		fi
 
-			# Begin Ping Sweep 
-			echo "Active Hosts"
-			echo "------------"
-			
-			for i in $(seq $START $END 2> /dev/null); do
-				HOST=$(echo $1.$i)
-					fping -c1 -t300 $HOST 2> /dev/null 1> /dev/null
-					# fping's -t option is in miliseconds and can be modified to take loner or shorter. My goal here is speed.
-					if [ "$?" = 0 ]
-					then
-						echo $HOST
-					fi
-				done
+		# Begin Ping Sweep 
+		echo "Active Hosts"
+		echo "------------"
+		
+		for i in $(seq $START $END 2> /dev/null); do
+			HOST=$(echo $1.$i)
+				fping -c1 -t300 $HOST 2> /dev/null 1> /dev/null
+				# fping's -t option is in miliseconds and can be modified to take loner or shorter. My goal here is speed.
+				if [ "$?" = 0 ]
+				then
+					echo $HOST
+				fi
+			done
 	fi
 fi
