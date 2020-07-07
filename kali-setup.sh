b9d88280aaa559441174e62a0ca6c3c3cf4cb2b4
tobor <rosborne@osbornepro.com>
2020-07-07 14:08:38 -0600

    Update kali-setup.sh

--- kali-setup.sh
+++ kali-setup.sh
@@ -184,6 +184,7 @@ cd /usr/share/
 sudo git clone https://github.com/frohoff/ysoserial.git
 sudo git clone https://github.com/Tib3rius/AutoRecon.git
 sudo git clone https://github.com/skelsec/pypykatz.git
+python /usr/share/pypykatz/setup.py install
 sudo git clone https://github.com/arthaud/git-dumper.git
 sudo git clone https://github.com/trickster0/Enyx.git
 cp /usr/share/Enyx/enyx.py /usr/local/bin/enyx
