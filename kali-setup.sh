00552e4cced4e3f9a462f1e162732585e76dc561
tobor <rosborne@osbornepro.com>
2020-07-12 12:29:20 -0600

    Update kali-setup.sh

--- kali-setup.sh
+++ kali-setup.sh
@@ -74,6 +74,7 @@ sudo apt install beef -y
 sudo apt install ncat -y
 sudo apt install stoken -y
 sudo apt install recon-ng -y
+sudp apt install maven -y
 sudo apt install brutespray -y
 sudo apt install checksec -y
 sudo apt install reaver -y
