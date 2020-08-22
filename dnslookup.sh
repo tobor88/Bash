821be12c2a7f4169220f7210454ecff262bf833e
tobor <rosborne@osbornepro.com>
2020-08-22 16:31:43 -0600

    Update dnslookup.sh
    Made Bash scripting more to standards

--- dnslookup.sh
+++ dnslookup.sh
@@ -1,88 +1,104 @@
 #!/bin/bash
-
 # This command is meant to easily perform a dns lookup for all hosts in a subnet range
 
-# Allow Ctrl+C to kill pingsweep
-
+# Allow Ctrl+C to kill dnslookup
 trap '
   trap - INT # restore default INT handler
-  /usr/bin/kill -s INT "$$"
+  kill -s INT "$$"
 ' INT
 
-if [ -z "$1" ]; then
-        /usr/bin/echo "Use -h switch to view help information"
-        /usr/bin/echo ""
 
-else
-        if [ "$1" == '-h' ] || [ "$1" == '--help' ] ; then
-                /usr/bin/echo ""
-                /usr/bin/echo "OsbornePro dnslookup 1.0 ( https://roberthosborne.com )"
-                /usr/bin/echo ""
-                /usr/bin/echo "USAGE: dnslookup [network <string format is #.#.#>] [int <start address>] [int <end address>]"
-                /usr/bin/echo ""
-                /usr/bin/echo "OPTIONS:"
-                /usr/bin/echo "  -h : Displays the help information for the command."
-                /usr/bin/echo " --help: Displays the help information for the command."
-                /usr/bin/echo ""
-                /usr/bin/echo "EXAMPLES:"
-                /usr/bin/echo "  dnslookup 192.168.0"
-		/usr/bin/echo "  This example performs a dns host lookup from 192.168.0.1 to 192.168.0.254"
-                /usr/bin/echo ""
-                /usr/bin/echo "  dnslookup 192.168.0 200"
-                /usr/bin/echo "  This example performs a dns host lookup from 192.168.0.200 to 192.168.0.254"
-                /usr/bin/echo ""
-                /usr/bin/echo "  dnslookup 192.168.0 128 192"
-                /usr/bin/echo "  This example performs a dns host lookup from 192.168.0.128 to 192.168.0.192"
-                /usr/bin/echo ""
-                exit
+USAGE="Syntax: dnslookup [-h] -i <network> [-s <int32 starting port>] [-e <int32 ending port>]
 
-	# Variable validation------------------------------------------------
-	elif [[ "$1" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] || ERROR="Valid IP subnet was not defined. For more help execute 'dnslookup -h' Example Value: 172.16.32 "; then
+OsbornePro dnslookup 2.0 ( https://roberthosborne.com )
+
+USAGE: dnslookup -i [network <string format is #.#.#>] 
+
+    OPTIONS:
+        -h : Displays the help information for the command.
+	-i : Set the network subnet to perform the dns lookups on
+	-s : Set the starting IP address to begin the lookups from
+	-e : Set the last IP Address to lookup
+
+    EXAMPLES:
+          dnslookup -i 192.168.0 
+            # This example performs a dns lookup from 192.168.0.1 to 192.168.0.254
+
+          dnslookup-i 192.168.0 -s 10
+            # This example performs a dns lookup from 192.168.0.10 to 192.168.0.254
+
+	  dnslookup -i 192.168.0 -s 1 -e 10
+	    # This example performs a dns lookup from 192.168.0.1 to 192.168.0.10
+	    
+	"
+
+
+function print_usage {
+	printf "$USAGE\n" >&2
+	exit 1
+    }  # End function print_usage	
+
+
+while [ ! -z "$1" ]; do
+	case "$1" in
+		-i) 
+			shift
+		   	ipv4=$1
+			;;
+		-s) 
+			shift
+		   	start=$1
+			;;
+		-e) 
+			shift
+		   	end=$1
+			;;
+		*) 
+			print_usage
+			;;
+	esac
+shift
+done
+
+
+if [[ "$ipv4" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] || ERROR="Valid IP subnet was not defined. For more help execute 'dnslookup -h' Example Value: 172.16.32 "; then
+
 		# Validate first parameter was defined correctly
-		if [ ! $ERROR ]; then
-			# Validate correct amount of positional parameters are defined
-			if [ -n "$4" ]; then
-				/usr/bin/echo "Too many positional parameters have been defined. Execute 'pingsweep -h' for more options"
-				/usr/bin/echo ""
-				exit
-			fi
-		else
-			/usr/bin/echo $ERROR
-			/usr/bin/echo ""
-			exit
+		if [ "$ERROR" ]; then
+			printf "$ERROR\n"
+			exit 1
 		fi
 
-		# Validate positional parameter 2 is an integer between 1 and 254
-		if [ -z "$2" ]; then
+		# Validate parameter $START is an integer between 1 and 254
+		if [ -z "$start" ]; then
 			START=1
-		elif [ "$2" -lt 255 ] && [ "$2" -ge 1 ] || ERROR="Second parameter needs to be an integer between 1 and 254"; then
+		elif [ "$start" -lt 255 ] && [ "$start" -ge 1 ] || ERROR="Start parameter needs to be an integer between 1 and 254"; then
 			if [ ! $ERROR ]; then
-				START=$2
+				START=$start
 			else
-				/usr/bin/echo $ERROR
-				/usr/bin/echo ""
-				exit
+				printf "$ERROR\n"
+				exit 1
 			fi
 		fi
 			
-		# Validate positional parameter 3 is an integer between $2 and 254
-		if [ -z "$3" ]; then
+		# Validate parameter $END is an integer between $START and 254
+		if [ -z "$end" ]; then
 			END=254
-		elif [ "$3" -lt 255 ] && [ "$3" -gt "$2" ] || ERROR="Third parameter needs to be an integer between the value of positional parameter two and 254"; then
+		elif [ "$end" -lt 255 ] && [ "$end" -gt "$start" ] || ERROR="End parameter needs to be an integer between the value of positional parameter two and 254"; then
 			if [ ! $ERROR ]; then
-				END=$3
+				END=$end
 			else
-				/usr/bin/echo $ERROR
-				/usr/bin/echo ""
-				exit
+				printf "$ERROR\n"
+				exit 1
 			fi
 		fi
 
 		# Begin DNS Lookups
-		echo "--------------------------------------------"
-		echo "| IP Address              |     FQDN's     |"
-		echo "--------------------------------------------"
+		printf "%s\n---------------------------------------------------"
+		printf "%s\n| IP Address             |         FQDN's         |"
+		printf "%s\n---------------------------------------------------%s\n"
 		
-		for i in $(/usr/bin/seq $START $END); do /usr/bin/host $1.$i; done | /usr/bin/grep -v "not found" | /usr/bin/awk {'print $1, $5'} | /usr/bin/sed 's/".in-addr.arpa "/" | "/'
-	fi
+		for i in $(seq $START $END); do 
+			host $ipv4.$i
+		done | grep -v "not found" | awk {'print $1, $5'} | sed 's/".in-addr.arpa "/" | "/'
 fi
