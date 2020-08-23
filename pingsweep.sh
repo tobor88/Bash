19cec42c1a4e2823de23961e73cfaca88a07542f
tobor <rosborne@osbornepro.com>
2020-08-22 23:54:41 -0600

    Update pingsweep.sh
    Added more functions and changed the command used for validating the existence of fping

--- pingsweep.sh
+++ pingsweep.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
 
-# Allow Ctrl+C to kill pingsweep
-trap '
-  trap - INT # restore default INT handler
-  kill -s INT "$$"
-' INT
-
-
+PINGCMD=$(command -v fping)
 USAGE="Syntax: $0 [-h] -i <network> [-s <int32 starting port>] [-e <int32 ending port>]
 
-OsbornePro pingsweep 2.0 ( https://roberthosborne.com )
+OsbornePro pingsweep 2.1 ( https://roberthosborne.com )
 NOTE: This command is most efficient on Linux distros that have fping installed
 
 Usage: pingsweep -i <string format is #.#.#> [[-s <start address>] [-e <end address>]]
@@ -31,18 +25,109 @@ Usage: pingsweep -i <string format is #.#.#> [[-s <start address>] [-e <end addr
 	  pingsweep -i 192.168.0 -s 128 -e 192
 	    # This example performs a ping sweep from 192.168.0.128 to 192.168.0.192
 	    
-	    "
+"
+
+
+function get_cmd {
+
+	# Determines whether fping or ping will be used
+	if [ -z $PINGCMD ]; then
+		CMD="ping -s 16 -c 1 -i 1 -U -W 1 -4 \$HOST 2> /dev/null"
+	else
+		CMD="fping -c1 -t300 \$HOST 2> /dev/null 1> /dev/null"
+	fi
+	
+}  # End function get_cmd
+
+
+function allow_ctrlc {
+
+	# Allow Ctrl+C to kill pingsweep
+	trap '
+	  trap - INT # restore default INT handler
+	  kill -s INT "$$"
+	' INT
+
+}  # End function allow_ctrlc
+
 
 function print_usage {
+
 	printf "$USAGE\n" >&2
 	exit 1
+
     }  # End function print_usage	
+
+
+function validate_start {
+	
+	# Validate start parameter is an integer between 1 and 254
+	if [ -z "$start" ]; then
+		START=1
+	elif [ "$start" -lt 255 ] && [ "$start" -ge 1 ] || ERROR="Start parameter needs to be an integer between 1 and 254"; then
+		if [ ! $ERROR ]; then
+			START=$start
+		else
+			printf "$ERROR\n"
+			exit
+		fi
+	fi
+
+}  # End function validate_start
+
+
+function validate_end {
+	
+	# Validate positional parameter 3 is an integer between $2 and 254
+	if [ -z "$end" ]; then
+		END=254
+	elif [ "$end" -lt 255 ] && [ "$end" -gt "$start" ] || ERROR="Last ending IP address needs to be an integer between the value of $start and 254"; then
+		if [ ! $ERROR ]; then
+			END=$end
+		else
+			printf "$ERROR\n"
+			exit
+		fi
+	fi
+
+}  # End function validate_end
+
+
+function validate_ipv4 {
+
+	if [[ "$ipv4" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] || ERROR="Valid IP subnet was not defined. For more help execute 'pingsweep -h' Example 172.16.32 "; then
+		if [ -n "$ERROR" ]; then
+			printf "[x] A valid network value was not defined. Used -h for more info. Example: 172.16.0\n"
+			exit 1
+		fi
+	fi
+
+}  # End function validate_ipv4
+
+
+function execute_pingsweep {
+
+	printf "[*] Starting Ping Sweep\n"
+	echo -e "------------\nActive Hosts\n------------"
+
+	for i in $(seq $START $END 2> /dev/null); do
+		HOST=$(echo $ipv4.$i)
+			eval $CMD
+			if [ "$?" = 0 ]; then
+				printf "$HOST\n"
+			fi
+	done
+
+	printf "[*] Ping Sweep execution completed\n"
+
+}  # End function execute_pingsweep
+
 
 
 while [ ! -z "$1" ]; do
 	case "$1" in
 		-i)
-		        shift	
+		        shift
 			ipv4=$1
 			;;
 		-s)
@@ -53,66 +138,16 @@ while [ ! -z "$1" ]; do
 			shift
 			end=$1
 			;;
-		-h) 
-			print_usage
-			;;
-		*) 
+		*)
 			print_usage
 			;;
 	esac
 shift
 done
 
-
-if [[ "$ipv4" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] || ERROR="Valid IP subnet was not defined. For more help execute 'pingsweep -h' Example 172.16.32 "; then
-
-	# Validate start parameter is an integer between 1 and 254
-	if [ -z "$start" ]; then
-		START=1
-	elif [ "$start" -lt 255 ] && [ "$start" -ge 1 ] || ERROR="Second parameter needs to be an integer between 1 and 254"; then
-		if [ ! $ERROR ]; then
-			START=$start
-		else
-			printf "$ERROR\n"
-			exit
-		fi
-	fi
-
-
-	# Validate positional parameter 3 is an integer between $2 and 254
-	if [ -z "$end" ]; then
-		END=254
-	elif [ "$end" -lt 255 ] && [ "$end" -gt "$start" ] || ERROR="Last IP address needs to be an integer between the value of the $start parameter and 254"; then
-		if [ ! $ERROR ]; then
-			END=$end
-		else
-			printf "$ERROR\n"
-			exit
-		fi
-	fi
-
-
-	printf "[*] Starting Ping Sweep\n"
-	echo -e "------------\nActive Hosts\n------------"
-		
-	PINGCMD=$(which fping)
-		
-	if [ -z $PINGCMD ]; then
-		for i in $(seq $START $END 2> /dev/null); do
-			HOST=$(echo $ipv4.$i)
-				ping -s 16 -c 1 -i 1 -U -W 1 -4 $HOST 2> /dev/null
-				if [ "$?" = 0 ]; then
-					printf "$HOST\n"
-				fi
-			done
-	else
-		for i in $(seq $START $END 2> /dev/null); do
-			HOST=$(echo $ipv4.$i)
-			fping -c1 -t300 $HOST 2> /dev/null 1> /dev/null
-			# fping's -t option is in miliseconds and can be modified to take loner or shorter. My goal here is speed.
-			if [ "$?" = 0 ]; then
-				echo $HOST
-			fi
-		done
-	fi
-fi
+get_cmd
+allow_ctrlc
+validate_ipv4
+validate_start
+validate_end
+execute_pingsweep
