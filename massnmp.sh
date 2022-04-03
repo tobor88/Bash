e094f1c12b2569360901d86f534d68e70accf5da
tobor <rosborne@osbornepro.com>
2022-04-02 23:58:36 -0600

    Added more switch parameters and functionality
    Added ability to defined the SNMP version, ability to define a community file list, and checks for required tools with how to install them.  If the community list is left blank a custom one will be built for use that contains default values based on different devices

--- massnmp.sh
+++ massnmp.sh
@@ -1,118 +1,255 @@
 #!/bin/bash
-
-# This script is meant to enumerate snmpv1 and snmpv2c in a subnet range you define
+#=================#
+# MASSNMP SCANNER #
+#=================#
+# This script is meant to enumerate snmpv1 and snmpv2c in a subnet range you define or a single host
 #
-# I wrote this script on Linux kali 5.4.0-kali4-amd64 #1 SMP Debian 5.4.19-1kali1 (2020-02-17) x86_64 GNU/Linux while studying for the OSCP. 
+# REQUIRES: snmp-check and onesixtyone. 
+#--------------------------------------------------------
+# Install these tools if you do not have them installed.
+#--------------------------------------------------------
+#   Debian/Ubuntu/Mint/Kali
+#     sudo apt-get -y install onesixtyone snmpcheck 
 #
-# As such it uses snmp-check and onesixtyone. Installs these tools if you do not have them installed.
-#     sudo apt-get -y install onesixtyone
-#     sudo apt-get -y install snmp-check 
-#
-# If you are a poser who uses Parrot ;) then just remove the absoulte paths to commands.
-#     sed -i 's|/usr/bin|""|g' masscan.sh
+#   Fedora/RHEL/CentOS
+#	sudo dnf -y install onesixtyone
+#	sudo wget http://www.nothink.org/codes/snmpcheck/snmpcheck-1.9.rb
+#	sudo cp snmpcheck-1.9.rb /usr/local/bin/snmp-check; sudo chmod a+x /usr/local/bin/snmp-check
+#	sudo gem install snmp
+
+TMPPATH="/tmp"
+IPV4REGEX="^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$"
+USAGE="Syntax: $0 [-h] -n <network> [[[-s <int32: solo/start address>]] [-e <int32: end address>]] -v <int32: <snmp version> [-f <string: community string file>]
+
+OsbornePro massnmp v1.1 ( https://osbornepro.com )
+
+IMPORTANT!!!
+    This command requires the tools 'onesixtyone' and 'snmpcheck'
+    Installs these tools if you do not have them installed.
+
+    USAGE: massnmp -n <string format is #.#.#> [[[-s <solo/start address>]] [-e <end address>]] -v <int32: <snmp version> [-f <string: community string list file>]
+
+    OPTIONS:
+        -h : Displays the help information for the command.
+	-n : Set the network subnet to perform the ping sweep on
+	-s : Set the solo or starting IP Address to begin the scan from
+	-e : Set the ending IP Address to scan too
+	-v : Define the version of SNMP to use [1|2c]
+	-f : Define the path to a file containing a list of community strings. 
+		(If -f is not defined common community string names will be tried)
+
+    EXAMPLES:
+          massnmp -n 192.168.0 -v 2c
+            # This example scans SNMP version 2c from 192.168.0.1 to 192.168.0.254 testing 121 common community strings
+
+          massnmp -n 192.168.0 -s 200 -v1
+            # This example scans SNMP version 1 on 192.168.0.200
+
+	  massnmp -n 192.168.0 -s 128 -e 192 -v 2c -f /tmp/community.txt
+	    # This example scans SNMP version 2c from 192.168.0.128 to 192.168.0.192 using a custom list in /tmp/community.txt
+
+"
 
 
-# Allow Ctrl+C to kill massnmp
-trap '
-  trap - INT # restore default INT handler
-  kill -s INT "$$"
-' INT
 
-if [ -z "$1" ]; then
-	/usr/bin/printf "Use -h switch to view help information\n\n"
-else
-	if [ "$1" == '-h' ] || [ "$1" == '--help' ] ; then
-		# This option displays a help message and command execution examples
-		/usr/bin/printf "\n"
-		/usr/bin/printf "OsbornePro massnmp 1.0 ( https://roberthosborne.com )\n"
-		/usr/bin/printf "\n"
-		/usr/bin/printf "USAGE: massnmp [network <string format is #.#.#>] [int <start address>] [int <end address>]\n"
-		/usr/bin/printf "\n"
-		/usr/bin/printf "OPTIONS:\n"
-		/usr/bin/printf "  -h : Displays the help information for the command.\n"
-		/usr/bin/printf "\n"
-		/usr/bin/printf "EXAMPLES:\n"
-		/usr/bin/printf "  massnmp 192.168.0\n"
-		/usr/bin/printf "  # This example performs an SNMP sweep from 192.168.0.1 to 192.168.0.254\n"
-		/usr/bin/printf "\n"
-		/usr/bin/printf "  massnmp 192.168.0 200\n"
-		/usr/bin/printf "  # This example performs an SNMP sweep from 192.168.0.200 to 192.168.0.254\n"
-		/usr/bin/printf "\n"
-		/usr/bin/printf "  massnmp 192.168.0 128 192\n"
-		/usr/bin/printf "  # This example performs an SNMP sweep from 192.168.0.128 to 192.168.0.192\n"
-		/usr/bin/printf "\n"
-		exit
+function allow_ctrlc {
 
-# Variable validation------------------------------------------------
-	elif [[ "$1" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] || ERROR="Valid IP subnet was not defined. For more help execute 'massnmp -h' Example 172.16.32 "; then
-		
-		# Validate first parameter was defined correctly
-		if [ ! $ERROR ]; then
-			# Validate correct amount of positional parameters are defined
-			if [ -n "$4" ]; then
-				/usr/bin/printf "Too many positional parameters have been defined. Execute 'massnmp -h' for more options\n\n"
-				exit
-			fi
-		else
-			/usr/bin/printf "$ERROR\n"
-			exit
-		fi
+	# Allow Ctrl+C to kill massnmp
+	trap '
+	  trap - INT # restore default INT handler
+	  kill -s INT "$$"
+	' INT
 
-		# Validate positional parameter 2 is an integer between 1 and 254
-		if [ -z "$2" ]; then
-			START=1
-		elif [ "$2" -lt 255 ] && [ "$2" -ge 1 ] || ERROR="Second parameter needs to be an integer between 1 and 254"; then
-			if [ ! $ERROR ]; then
-				START=$2
-			else
-				/usr/bin/printf "$ERROR\n\n"
-				exit
-			fi
-		fi
-			
-		# Validate positional parameter 3 is an integer between $2 and 254
-		if [ -z "$3" ]; then
-			END=254
-		elif [ "$3" -lt 255 ] && [ "$3" -gt "$2" ] || ERROR="Third parameter needs to be an integer between the value of positional parameter two and 254"; then
-			if [ ! $ERROR ]; then
-				END=$3
-			else
-				/usr/bin/printf "$ERROR\n\n"
-				exit
-			fi
-		fi
+}  # End function allow_ctrlc
 
-		# Create community list
-		if [ ! -f /tmp/community.list ]; then
-			/usr/bin/printf "[i] I would suggest using FuzzDB's wordlist-common-snmp-community-strings.txt for the best results.\n"
-			/usr/bin/printf "[i] To do this copy that wordlist to the /tmp directory and rename it too community.list\n\n"
-			/usr/bin/echo $'private\npublic\nmanager' >> /tmp/community.list
-		else
-			/usr/bin/printf "[*] Wordlist $PWD/community.list has been found to already exist\n"
-		fi
 
-		# Create file containing desired ipv4 address range if it doesnt exist
-		/usr/bin/printf "[*] Creating ip.list file using the range you defined\n"
-		for ip in $(seq $START $END); do 
-		    /usr/bin/echo $1.$ip
-		done  > /tmp/ip.list
+function print_usage {
 
-		# Discovering SNMP enabled hosts
-		/usr/bin/printf "[*] Discovering SNMP enabled hosts\n"
-		/usr/bin/onesixtyone -c /tmp/community.list -i /tmp/ip.list -o /tmp/snmp_hosts.txt
-		
-		/usr/bin/printf "--------------------\n"
-		/usr/bin/printf " SNMP Enabled Hosts \n"
-		/usr/bin/printf "--------------------\n"
-		
-		/usr/bin/cat /tmp/snmp_hosts.txt | /usr/bin/awk {'print $1'}
+	printf "$USAGE\n" >&2
+	exit 1
 
-		for i in $(/usr/bin/cat /tmp/snmp_hosts.txt | /usr/bin/awk {'print $1'}); do 
-			IP=($(/usr/bin/cat /tmp/snmp_hosts.txt | /usr/bin/awk {'print $1'}))
-			COMMUNITY=($(/usr/bin/cat /tmp/snmp_hosts.txt | /usr/bin/awk {'print $2'} | /usr/bin/tr -d '[]'))
+}  # End function print_usage	
 
-			/usr/bin/snmp-check -c $COMMUNITY -p 161 -v1 $IP > "$i.txt" 2>&1 || /usr/bin/snmp-check -c $COMMUNITY -p 161 -v2c $IP > "$i.txt" 2>&1
-		done
-		/usr/bin/printf "[*] Text files created for each ip address contain enumerated SNMPv1 or 2c information.\n"
+
+function verify_tools {
+	# Ensure the required binaries are usable
+	if ! command -v onesixtyone &> /dev/null; then
+		printf "[x] Requires onesixtyone and snmp-check to run. 
+Install using below commands:
+
+  Debian/Ubuntu/Mint/Kali
+    CMD:  sudo apt-get -y install onesixtyone snmpcheck 
+
+  Fedora/RHEL/CentOS
+    CMD: sudo wget http://www.nothink.org/codes/snmpcheck/snmpcheck-1.9.rb
+    CMD: sudo cp snmpcheck-1.9.rb /usr/local/bin/snmp-check; sudo chmod a+x /usr/local/bin/snmp-check
+    CMD: sudo gem install snmp\n"
+		    exit 1
+	elif ! command -v snmp-check &> /dev/null; then
+		printf "[x] Requires snmp-check to run. 
+Install using below commands
+
+  Debian/Ubuntu/Mint/Kali
+    CMD:  sudo apt-get -y install snmpcheck 
+
+  Fedora/RHEL/CentOS
+    CMD: sudo wget http://www.nothink.org/codes/snmpcheck/snmpcheck-1.9.rb
+    CMD: sudo cp snmpcheck-1.9.rb /usr/local/bin/snmp-check; sudo chmod a+x /usr/local/bin/snmp-check
+    CMD: sudo gem install snmp\n"
+		exit 1
+	else
+		return 1
 	fi
-fi
+
+}
+
+function validate_start {
+	
+	# Validate start parameter is an integer between 1 and 254
+	if [ -z "$start" ]; then
+		START=1
+	elif [ "$start" -lt 255 ] && [ "$start" -ge 1 ] || ERROR="Start parameter needs to be an integer between 1 and 254"; then
+		if [ ! $ERROR ]; then
+			START=$start
+		else
+			printf "$ERROR\n"
+			exit 1
+		fi
+	fi
+
+}  # End function validate_start
+
+
+function validate_end {
+	# Validate positional parameter 3 is an integer between $2 and 254
+	if [ -z $end ] && [ -z $start ] ; then
+		END=254
+	elif [ -z $end ] && [ -n $start ]; then
+		END=$start
+	elif [ "$end" -lt 255 ] && [ "$end" -ge "$start" ] || ERROR="Last ending IP address needs to be an integer between the value of $start and 254"; then
+		if [ ! $ERROR ]; then
+			END=$end
+		else
+			printf "$ERROR\n"
+			exit 1
+		fi
+	fi
+
+}  # End function validate_end
+
+
+function validate_ipv4 {
+	# Verifying $ipv4 value"
+	if [[ "$ipv4" =~ "$IPV4REGEX" ]] || IPERROR="Valid IP subnet was not defined. For more help execute 'massnmp -h' Example 172.16.32 "; then
+		if [ ! "$IPERROR" ]; then
+			printf "[x] A valid network value was not defined, you entered $ipv4. Used -h for more info. Example: 172.16.0\n"
+			exit 1
+		fi
+	fi
+
+}  # End function validate_ipv4
+
+
+function validate_snmp_version {
+	# Ensures a valid SNMP version was selected
+	case $version in
+		"1")
+			printf "[*] Using SNMP version $version \n"
+			;;
+		"2c")	printf "[*] Using SNMP version $version \n"
+			;;
+		*)
+			printf "[x] SNMP version (-v) must be 1 or 2c\n"
+		        exit 1	
+			;;
+	esac
+}
+
+
+function create_community_list {
+	# Create community list
+	if [ ! -f "$file" ]; then
+		CLIST="/tmp/community.lst"
+		echo $'private\npublic\nmanagerpublic\n0\n0392a0\n1234\n2read\n4changes\nANYCOM\nAdmin\nC0de\nCISCO\nCR52401\nIBM\nILMI\nIntermec\nNoGaH$@!\nOrigEquipMfr\nPRIVATE\nPUBLIC\nPrivate\nPublic\nSECRET\nSECURITY\nSNMP\nSNMP_trap\nSUN\nSWITCH\nSYSTEM\nSecret\nSecurity\ns!a@m#n$p%c\nSwitch\nSystem\nTENmanUFactOryPOWER\nTEST\naccess\nadm\nadmin\nagent\nagent_steal\nall\nall private\nall public\napc\nbintec\nblue\nc\ncable-d\ncanon_admin\ncc\ncisco\ncommunity\ncore\ndebug\ndefault\ndilbert\nenable\nfield\nfield-service\nfreekevin\nfubar\nguest\nhello\nhp_admin\nibm\nilmi\nintermec\ninternal\nl2\nl3\nmanager\nmngt\nmonitor\nnetman\nnetwork\nnone\nopenview\npass\npassword\npr1v4t3\nproxy\npubl1c\nread\nread-only\nread-write\nreadwrite\nred\nregional\nrmon\nrmon_admin\nro\nroot\nrouter\nrw\nrwa\nsan-fran\nsanfran\nscotty\nsecret\nsecurity\nseri\nsnmp\nsnmpd\nsnmptrap\nsolaris\nsun\nsuperuser\nswitch\nsystem\ntech\ntest\ntest2\ntiv0li\ntivoli\ntrap\nword\nwrite\nxyzzy\nyellow' > $CLIST
+		printf "[*] Using 121 of the most common SNMP community strings defined in $CLIST\n"
+	else
+		CLIST="$file"
+		printf "[*] Using the community list defined in $CLIST\n"
+	fi
+}
+
+
+function create_ipv4_list {
+	# Create file containing desired ipv4 address range if it doesnt exist
+	for ip in $(seq $START $END); do 
+	    echo $ipv4.$ip
+	done  > /tmp/ip.lst
+}
+
+
+function discover_snmp_hosts {
+	# Discovering SNMP enabled hosts
+	printf "[*] Discovering SNMP enabled hosts\n"
+	onesixtyone -c "$CLIST" -i "$TMPPATH/ip.lst" -o "$TMPPATH/snmp_hosts.txt" 1>/dev/null
+		
+	SNMP_HOSTS=$(cat "$TMPPATH/snmp_hosts.txt" | awk {'print $1 $2'} | sort | uniq)
+	printf "\n#====================#\n|   SNMP Discovery   |\n#====================#\n"
+	echo $SNMP_HOSTS
+
+	printf "\nEnumerating SNMP info from discovered devices. Please wait...\n"
+	for i in $(/usr/bin/cat /tmp/snmp_hosts.txt | /usr/bin/awk {'print $1'}); do 
+		IP=($(/usr/bin/cat /tmp/snmp_hosts.txt | /usr/bin/awk {'print $1'}))
+		COMMUNITY=($(cat "$TMPPATH/snmp_hosts.txt" | awk {'print $2'} | tr -d '[]'))
+
+		snmp-check -c $COMMUNITY -p 161 -v1 $IP > "$i-snmp$version.txt" 2>&1 || snmp-check -c $COMMUNITY -p 161 -v2c $IP > "$i-snmp$version.txt" 2>&1
+	done
+
+	printf "\n[*] OneSixtyOne Results\n"
+	cat "$TMPPATH/snmp_hosts.txt" | sort | uniq
+	printf "\n[*] View Detailed SNMP Info in Below Files\n"
+	ls *-snmp$version.txt
+}
+
+function remove_files {
+	# Removes files that were created to run commands
+	rm -rf "$TMPPATH/ip.lst" "$TMPPATH/snmp_hosts.txt"
+}
+
+
+while [ ! -z "$1" ]; do
+	case "$1" in
+		-n)
+		        shift
+			ipv4=$1
+			;;
+		-s)
+			shift
+			start=$1
+			;;
+		-e)
+			shift
+			end=$1
+			;;
+		-v)	shift
+			version=$1
+			;;
+		-f)	shift
+			file=$1
+			;;
+		*)
+			print_usage
+			;;
+	esac
+shift
+done
+
+
+allow_ctrlc
+verify_tools
+validate_ipv4
+validate_start
+validate_end
+validate_snmp_version
+create_community_list
+create_ipv4_list
+discover_snmp_hosts
